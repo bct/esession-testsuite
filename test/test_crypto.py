4cbdab65ca3a9ff5fadff18bf17ec5e3c2005af3
Brendan Taylor <whateley@gmail.com>
2007-07-09 14:55:32 -0600

    test_crypto uses esession.py
    darcs-hash:20070709205532-ce558-84acbe890a2f2736cb2d56ddef43346b8227aba4.gz

--- test/test_crypto.py
+++ test/test_crypto.py
@@ -8,135 +8,29 @@ from Crypto.Hash import HMAC, SHA256
 import xmpp
 import base64
 
+import sys
+
+sys.path.append('..')
+
+from esession import *
+
 class DecryptionError(RuntimeError):
   pass
 
-class Party:
-  def encode_mpi(self, n):
-    if n >= 256:
-      return self.encode_mpi(n / 256) + chr(n % 256)
-    else:
-      return chr(n)
-
-  def encode_mpi_with_padding(self, n):
-    ret = self.encode_mpi(n)
-
-    mod = len(ret) % 16
-    if mod != 0:
-      ret = ((16 - mod) * '\x00') + ret
-
-    return ret
-
-  def decode_mpi(self, s):
-    if len(s) == 0:
-      return 0
-    else:
-      return 256 * self.decode_mpi(s[:-1]) + ord(s[-1])
-
+class Party(ESession):
   def __init__(self, en_key, de_key, en_counter, de_counter):
-    self.n = 128   # number of bits
+    ESession.__init__(self, None, None, None, None, None)
+
     bytes = self.n / 8
 
     if not len(en_key) == bytes and len(de_key) == bytes:
       raise 'wrong key length'
 
-    self.en_key = en_key 
-    self.de_key = de_key
+    self.kc_s = en_key
+    self.kc_o = de_key
 
-    self.en_counter = en_counter
-    self.de_counter = de_counter
-
-    self.cipher = AES
-    self.hash_alg = SHA256
-
-    self.encrypter = self.cipher.new(self.en_key, self.cipher.MODE_CTR, counter=self.encryptcounter)
-    self.decrypter = self.cipher.new(self.de_key, self.cipher.MODE_CTR, counter=self.decryptcounter)
-
-    self.compression = None
-
-  def encryptcounter(self):
-    self.en_counter = (self.en_counter + 1) % 2 ** self.n
-    return self.encode_mpi_with_padding(self.en_counter)
-
-  def decryptcounter(self):
-    self.de_counter = (self.de_counter + 1) % 2 ** self.n
-    return self.encode_mpi_with_padding(self.de_counter)
-
-  def encrypt_stanza(self, stanza):
-    encryptable = filter(lambda x: x.getName() not in ('error', 'amp', 'thread'), stanza.getChildren())
-
-    # XXX can also encrypt contents of <error/> elements in stanzas @type = 'error'
-    # (except for <defined-condition xmlns='urn:ietf:params:xml:ns:xmpp-stanzas'/> child elements)
-
-    old_en_counter = self.en_counter
-
-    for element in encryptable:
-      stanza.delChild(element)
-
-    plaintext = ''.join(map(str, encryptable))
-
-    compressed = self.compress(plaintext)
-    m_final = self.encrypt(compressed)
-
-    c = stanza.NT.c
-    c.setNamespace('http://www.xmpp.org/extensions/xep-0200.html#ns')
-    c.NT.data = base64.b64encode(m_final)
-
-    # XXX check for rekey, handle <key/> elements
-
-    m_content = ''.join(map(str, c.getChildren()))
-    c.NT.mac = base64.b64encode(self.hmac(m_content, old_en_counter, self.en_key))
-
-    return stanza
-
-  def hmac(self, content, counter, key):
-    return HMAC.new(key, content + self.encode_mpi_with_padding(counter), self.hash_alg).digest()
-
-  def compress(self, plaintext):
-    if self.compression == None:
-      return plaintext
-
-  def decompress(self, compressed):
-    if self.compression == None:
-      return compressed 
-
-  def encrypt(self, encryptable):
-    # XXX spec says this shouldn't require padding, but this library requires it
-    len_padding = 16 - (len(encryptable) % 16)
-    encryptable += len_padding * ' '
-
-    return self.encrypter.encrypt(encryptable)
-
-  def decrypt_stanza(self, stanza):
-    c = stanza.getTag(name='c', namespace='http://www.xmpp.org/extensions/xep-0200.html#ns')
-
-    stanza.delChild(c)
-
-    # contents of <c>, minus <mac>, minus whitespace
-    macable = ''.join(map(str, filter(lambda x: x.getName() != 'mac', c.getChildren())))
-
-    received_mac = base64.b64decode(c.getTagData('mac'))
-    calculated_mac = self.hmac(macable, self.de_counter, self.de_key)
-
-    if not calculated_mac == received_mac:
-      raise 'bad signature (%s != %s)' % (repr(received_mac), repr(calculated_mac))
-
-    m_final = base64.b64decode(c.getTagData('data'))
-    m_compressed = self.decrypt(m_final)
-    plaintext = self.decompress(m_compressed)
-
-    try:
-      parsed = xmpp.Node(node='<node>' + plaintext + '</node>')
-    except:
-      raise DecryptionError
-
-    for child in parsed.getChildren():
-      stanza.addChild(node=child)
-
-    return stanza
-
-  def decrypt(self, ciphertext):
-    return self.decrypter.decrypt(ciphertext)
+    self.c_s = en_counter
+    self.c_o = de_counter
 
 class TestCanonicalization(unittest.TestCase):
   def test_encrypt_decrypt(self):
@@ -170,7 +64,13 @@ class TestCanonicalization(unittest.TestCase):
   </amp>
   <active xmlns='http://jabber.org/protocol/chatstates'/>
 </message>''')
-  
+
+    alice.km_s = a_key
+    alice.km_o = b_key
+
+    bob.km_s = b_key
+    bob.km_o = a_key
+
     encrypted = alice.encrypt_stanza(msg)
 
     self.assert_(isinstance(encrypted, xmpp.Message))
